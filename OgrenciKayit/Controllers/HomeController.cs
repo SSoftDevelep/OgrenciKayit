f29b9e204d20ac704a10f206bbc53a095f1596ed
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-08 15:35:36 +0300

    Ogrenci Kayit MVC

---
+++ OgrenciKayit/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using OgrenciKayit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OgrenciKayit.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OgrenciKaydet(string OgrAd, string OgrSoyad, string DersAdi, int DersNotu)
+        {
+            if (HttpRuntime.Cache["Ogrenciler"]==null)
+            {
+                List<Ogrenci> OgrenciListesi = new List<Ogrenci>();
+                Ogrenci ogrenci = new Ogrenci();
+                ogrenci.Ad = OgrAd;
+                ogrenci.Soyad = OgrSoyad;
+                ogrenci.DersAd = DersAdi;
+                ogrenci.DersNot = DersNotu;
+
+                OgrenciListesi.Add(ogrenci);
+                HttpRuntime.Cache["Ogrenciler"] = OgrenciListesi; //ramde bir alan acip tutmak istedigimizi tutar.
+            }
+            else
+            {
+                List<Ogrenci> OgrenciListesi = (List<Ogrenci>)HttpRuntime.Cache["Ogrenciler"];
+                Ogrenci ogrenci = new Ogrenci();
+                ogrenci.Ad = OgrAd;
+                ogrenci.Soyad = OgrSoyad;
+                ogrenci.DersAd = DersAdi;
+                ogrenci.DersNot = DersNotu;
+
+                OgrenciListesi.Add(ogrenci);
+                HttpRuntime.Cache["Ogrenciler"] = OgrenciListesi;
+
+            }
+            
+            
+           
+
+            return RedirectToAction("OgrenciListesi");  //bu actiondan baska bir actiona yönlenme demek.
+
+        }
+
+        public ActionResult OgrenciListesi()
+        {
+            var model = (List<Ogrenci>)HttpRuntime.Cache["Ogrenciler"];
+            return View(model);
+        }
+    }
+}
---
+++ OgrenciKayit/Models/Ogrenci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OgrenciKayit.Models
+{
+    public class Ogrenci
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string DersAd { get; set; }
+        public int DersNot { get; set; }
+
+    }
+}
